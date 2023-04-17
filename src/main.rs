47cdba28e0c806427897cc02b23548e4dae956d5
Shrey Bhardwaj <shrey.bhardwaj@juspay.in>
2023-04-17 22:19:37 +0530

    concurrent code added for rendering

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,33 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    // uncomment next line and comment all the concurrent code to run on a single thread
+    // render(&mut pixels, bounds, upper_left, lower_right);
+    
+    // CODE TO RENDER CONCURRENTLY BEGINS
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+    crossbeam::scope(
+        |spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let (top, height) = (rows_per_band * i, band.len() / bounds.0);
+                let band_bounds = (bounds.0, height);
+
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawner.spawn(
+                    move |_| {
+                        render(band, band_bounds, band_upper_left, band_lower_right);
+                    }
+                );
+            }
+        }
+    ).unwrap();
+    
+    // CODE TO RENDER CONCURRENTLY ENDS
 
     write_image(&args[1], &pixels, bounds).expect("Error writing PNG file");
 }
